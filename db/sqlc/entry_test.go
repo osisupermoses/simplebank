3899912125929f51c3fdc9b4daf6cbc3348b33d5
Abiodun Osifowokan <osisuper_moses@yahoo.com>
2024-10-25 17:03:50 +0100

    initial commit

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/osisupermoses/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, accountID int64) Entry {
+	account := createRandomAccount(t)
+	require.NotEmpty(t, account)
+
+	var accID int64
+	if accountID != 0 {
+		accID = accountID
+	} else {
+		accID = account.ID
+	}
+	entryArg := CreateEntryParams{
+		AccountID: accID,
+		Amount: util.RandomMoney(),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), entryArg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	if accountID == 0 {
+		require.Equal(t, account.ID, entry.AccountID)
+	}
+
+	require.Equal(t, entryArg.AccountID, entry.AccountID)
+	require.Equal(t, entryArg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.CreatedAt)
+	
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t, 0)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t, 0)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	// create new account
+	acct := createRandomAccount(t)
+
+	// create 5 new entries with the same accountID
+	for range 5 {
+		createRandomEntry(t, acct.ID)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: acct.ID,
+		Limit: 3,
+		Offset: 2,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 3)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, entry.AccountID, acct.ID)
+	}
+}
---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/osisupermoses/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, from, to int64) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: from,
+		ToAccountID: to,
+		Amount: util.RandomMoney(),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, from)
+	require.Equal(t, transfer.ToAccountID, to)
+	require.Equal(t, transfer.Amount, arg.Amount)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	// create random from and to accounts
+	fromAcct := createRandomAccount(t)
+	toAcct := createRandomAccount(t)
+
+	createRandomTransfer(t, fromAcct.ID, toAcct.ID)
+}
+
+func TestListTransfers(t *testing.T) {
+	// create random from and to accounts
+	fromAcct := createRandomAccount(t)
+	toAcct := createRandomAccount(t)
+
+	for range 5 {
+		createRandomTransfer(t, fromAcct.ID, toAcct.ID)
+	}
+	arg := ListTransfersParams{
+		FromAccountID: fromAcct.ID,
+		ToAccountID: toAcct.ID,
+		Limit: 3,
+		Offset: 2,
+	}
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 3)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+		require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	}
+}
